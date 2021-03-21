bec6a7d3774497b23e485120a291b68ffc2c1a40
Nirav Sanghani <nirav.sanghani@gmail.com>
2021-03-21 12:24:26 -0700

    lint files, Context, firebase

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Data from './Data';
+import { Screen, Container } from './components';
+
+const App = () => {
+  return (
+    <Data>
+      <Container>
+        <Screen />
+      </Container>
+    </Data>
+  );
+};
+
+export default App;
---
+++ src/db.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const FIREBASE_CONFIG = {
+  apiKey: '***REMOVED***',
+  authDomain: 'gambit-app.firebaseapp.com',
+  projectId: 'gambit-app',
+};
+
+export default function initDb() {
+  firebase.initializeApp(FIREBASE_CONFIG);
+  return firebase.firestore();
+}
