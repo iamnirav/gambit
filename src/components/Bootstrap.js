a053ed6904616e78fc4ff51f89b2674eeb4cc348
Nirav Sanghani <nirav.sanghani@gmail.com>
2021-03-21 13:06:49 -0700

    header, data

--- src/components/Bootstrap.js
+++ src/components/Bootstrap.js
@@ -1,5 +1,12 @@
-export const Container = ({ children, ...props }) => (
-  <div className="container" {...props}>
+import classNames from 'classnames';
+
+const d = (name, { className, children, ...props }) => (
+  <div className={classNames(name, className)} {...props}>
     {children}
   </div>
 );
+
+export const Card = props => d('card', props);
+export const CardBody = props => d('card-body', props);
+export const Container = props =>
+  d(props.fluid ? 'container-fluid' : 'container', props);
