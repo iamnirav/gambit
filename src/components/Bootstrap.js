5c4e83015c7e9fac84131037fe41373a58826645
Nirav Sanghani <nirav.sanghani@gmail.com>
2021-03-23 00:17:18 -0700

    abilities

--- src/components/Bootstrap.js
+++ src/components/Bootstrap.js
@@ -17,12 +17,14 @@ export const CardBody = props => d('card-body', props);
 export const Container = ({ fluid, ...props }) =>
   d(fluid ? 'container-fluid' : 'container', props);
 
-export const Icon = ({ name }) => <i className={`bi bi-${name}`} />;
+export const Icon = ({ name, ...props }) => (
+  <i className={`bi bi-${name}`} {...props} />
+);
 
 export const Badge = ({ rounded, color, ...props }) =>
   s(`badge bg-${color} ${rounded ? 'rounded-pill' : ''}`, props);
 
-export const Checkbox = ({ checked, onChange, children, id }) => (
+export const Checkbox = ({ checked, onChange, children, id, disabled }) => (
   <div className="form-check">
     <input
       className="form-check-input"
@@ -30,6 +32,7 @@ export const Checkbox = ({ checked, onChange, children, id }) => (
       checked={checked}
       onChange={onChange}
       id={id}
+      disabled={disabled}
     />
     <label className="form-check-label" htmlFor={id}>
       {children}
@@ -37,17 +40,44 @@ export const Checkbox = ({ checked, onChange, children, id }) => (
   </div>
 );
 
-export const Progress = ({ now, max, children }) => (
-  <div className="progress">
-    <div
-      className="progress-bar"
-      role="progressbar"
-      aria-valuenow={now}
-      aria-valuemin="0"
-      aria-valuemax={max}
-      style={{ width: `${(now * 100) / max}%` }}
-    >
-      {children}
+export const Progress = ({ now, max, add }) => (
+  <div className="d-flex">
+    {add && (
+      <button
+        type="button"
+        className="btn btn-secondary btn-sm me-2"
+        disabled={now <= 0}
+        onClick={() => add(-1)}
+      >
+        <Icon name="chevron-left" />
+      </button>
+    )}
+
+    <div className="progress flex-fill" style={{ height: 32 }}>
+      <div
+        className={classNames('progress-bar', {
+          'bg-warning': now >= max - 2 && now < max,
+          'bg-danger': now === max,
+        })}
+        role="progressbar"
+        aria-valuenow={now}
+        aria-valuemin="0"
+        aria-valuemax={max}
+        style={{ width: `${(now * 100) / max}%` }}
+      >
+        {now}/{max}
+      </div>
     </div>
+
+    {add && (
+      <button
+        type="button"
+        className="btn btn-secondary btn-sm ms-2"
+        disabled={now >= max}
+        onClick={() => add(1)}
+      >
+        <Icon name="chevron-right" />
+      </button>
+    )}
   </div>
 );
