4240012afdf78489bff2d6f8f7f94d6af85503a9
Nirav Sanghani <nirav.sanghani@gmail.com>
2021-03-25 15:26:42 -0700

    character select screen

--- src/Data.js
+++ src/Data.js
@@ -6,38 +6,33 @@ import {
   useEffect,
 } from 'react';
 import initDb from './db';
-import INITIAL_STATE from './game/character';
-import PLAYBOOKS from './game/playbooks';
 
-export const CharacterContext = createContext(INITIAL_STATE);
-const db = initDb();
+export const CHAR_ID_STORAGE_KEY = 'recent-character-ids';
+export const CharacterContext = createContext({});
+export const db = initDb();
 
 const Data = props => {
-  const [character, setCharacter] = useState(INITIAL_STATE);
+  const [character, setCharacter] = useState({});
   const [charDocRef, setCharDocRef] = useState(undefined);
 
   useEffect(() => {
-    let id = window.location.pathname.slice(1);
+    const id = window.location.pathname.slice(1);
 
-    if (!id) {
-      const name = window.prompt('Enter new character name');
-      const playbook = window.prompt(
-        `Select playbook: ${PLAYBOOKS.join(', ')}`,
-      );
-      if (name && playbook) {
-        db.collection('characters')
-          .add({ ...INITIAL_STATE, name, playbook })
-          .then(doc => {
-            window.location = `/${doc.id}`;
-          });
-      }
-      return;
-    }
+    if (!id) return;
 
     const docRef = db.collection('characters').doc(id);
 
     docRef.get().then(doc => {
       if (doc.exists) {
+        const recentIds =
+          window.localStorage.getItem(CHAR_ID_STORAGE_KEY) || '';
+        if (!recentIds.includes(doc.id)) {
+          window.localStorage.setItem(
+            CHAR_ID_STORAGE_KEY,
+            `${recentIds} ${doc.id}`.trim(),
+          );
+        }
+
         setCharDocRef(docRef);
       } else {
         window.alert('No character found.');
