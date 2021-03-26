045f2ced1ff37dacf3fc806e7584652968173d71
Nirav Sanghani <nirav.sanghani@gmail.com>
2021-03-25 23:05:49 -0700

    playbook select, load by id

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -9,11 +9,14 @@ import { db, CHAR_ID_STORAGE_KEY } from '../Data';
 const CharacterSelect = () => {
   const { character } = useCharacter();
   const [recents, setRecents] = useState([]);
+  const [playbookSelect, setPlaybookSelect] = useState('placeholder');
+  const [idInput, setIdInput] = useState('');
 
   const recentIds = window.localStorage.getItem(CHAR_ID_STORAGE_KEY);
 
   useEffect(() => {
     if (recentIds) {
+      setRecents([]);
       recentIds.split(' ').forEach(id => {
         const docRef = db.collection('characters').doc(id);
 
@@ -34,7 +37,10 @@ const CharacterSelect = () => {
   return (
     <Container fluid className="CharacterSelect p-5">
       <h1 className="display-1">Gambit</h1>
-      <p>Select a recently opened character or create a new one.</p>
+      <p>
+        Select a recently opened character, create a new one, or load one if you
+        know their id.
+      </p>
       <div className="list-group">
         {recents.map(({ id, data }) => (
           <a
@@ -48,21 +54,67 @@ const CharacterSelect = () => {
             <Icon name="chevron-right" />
           </a>
         ))}
-        <div
-          className="list-group-item list-group-item-action d-flex justify-content-between"
-          onClick={createNew}
-        >
-          <span>Create new</span>
-          <Icon name="chevron-right" />
+        <div className="list-group-item list-group-item-action">
+          <span>Create new character…</span>
+          <select
+            className="form-select form-select-sm my-2"
+            aria-label="playbook selector"
+            value={playbookSelect}
+            onChange={evt => setPlaybookSelect(evt.target.value)}
+          >
+            <option value="placeholder">choose playbook</option>
+            {PLAYBOOKS.map(playbook => (
+              <option key={playbook} value={playbook}>
+                {playbook}
+              </option>
+            ))}
+          </select>
+          <button
+            className="btn btn-secondary"
+            onClick={() => createNew(playbookSelect)}
+            disabled={playbookSelect === 'placeholder'}
+          >
+            Create
+          </button>
+        </div>
+        <div className="list-group-item list-group-item-action">
+          <label htmlFor="idInput" className="form-label">
+            Load character by id…
+          </label>
+          <input
+            type="email"
+            className="form-control form-control-sm mb-2"
+            id="idInput"
+            placeholder="xxxxxxxxxxxxxxxxxxxx"
+            value={idInput}
+            onChange={evt => setIdInput(evt.target.value)}
+          />
+          <button
+            className="btn btn-secondary"
+            onClick={() => loadById(idInput)}
+            disabled={!idInput}
+          >
+            Load
+          </button>
         </div>
       </div>
     </Container>
   );
 };
 
-function createNew() {
+function loadById(id) {
+  db.collection('characters')
+    .doc(id)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        window.location = `/${doc.id}`;
+      }
+    });
+}
+
+function createNew(playbook) {
   const name = window.prompt('Enter new character name');
-  const playbook = window.prompt(`Select playbook: ${PLAYBOOKS.join(', ')}`);
   if (name && PLAYBOOKS.includes(playbook)) {
     db.collection('characters')
       .add({
